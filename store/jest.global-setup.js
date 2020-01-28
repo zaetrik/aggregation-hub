0dffecaaf3fee696c0474e80329fbe323a2e5ddc
Cedric Dose <cedricdose@Cedrics-MacBook-Pro.local>
2020-01-28 17:24:13 +0100

    updated

--- store/jest.global-setup.js
+++ store/jest.global-setup.js
@@ -22,5 +22,5 @@ module.exports = async () => {
   }
   shell.exec("docker-compose -f docker-compose.test.yml up --build -d");
 
-  await waitForServiceToBeUp("http://localhost:9200", 200);
+  await waitForServiceToBeUp("http://elasticsearch_1:9200", 200);
 };
