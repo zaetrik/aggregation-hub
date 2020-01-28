98e690803e1bd3468b74198007fa4269161194cf
Cedric Dose <cedricdose@Cedrics-MacBook-Pro.local>
2020-01-28 13:26:39 +0100

    added jest test setup

---
+++ store/jest.global-setup.js
@@ -0,0 +1,26 @@
+const shell = require("shelljs");
+
+const waitForServiceToBeUp = (url, httpCode) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(new Error("SERVICE NOT AVAILABLE"));
+    }, 60000);
+
+    setInterval(() => {
+      const response = shell.exec(
+        `curl -s -o /dev/null -w "%{http_code}" ${url}`
+      );
+      if (response == httpCode) resolve();
+    }, 1000);
+  });
+};
+
+module.exports = async () => {
+  if (!shell.which("docker-compose")) {
+    shell.echo("Sorry, this script requires docker-compose");
+    shell.exit(1);
+  }
+  shell.exec("docker-compose -f docker-compose.test.yml up -d");
+
+  await waitForServiceToBeUp("http://localhost:9200", 200);
+};
