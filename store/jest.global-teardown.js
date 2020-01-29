1eeae94af64e5bafa66cdd2d7d38905ad87ea018
Cedric Dose <cedricdose@Cedrics-MacBook-Pro.local>
2020-01-29 22:22:47 +0100

    ci

--- store/jest.global-teardown.js
+++ store/jest.global-teardown.js
@@ -1,7 +1,9 @@
 const shell = require("shelljs");
 
 module.exports = async () => {
-  shell.exec("docker-compose -f docker-compose.test.yml down");
-  shell.exec("rm -r elasticsearch-data-test");
-  shell.exec("docker system prune -f");
+  if (!process.env.CI) {
+    shell.exec("docker-compose -f docker-compose.test.yml down");
+    shell.exec("rm -r elasticsearch-data-test");
+    shell.exec("docker system prune -f");
+  }
 };
